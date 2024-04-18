0e17668847f362d54c77620ef4b2fee85746ff71
heriberto <haranda@heicommunity.com>
2024-04-17 20:47:15 -0600

    "Integracion navigator"

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@
  *
  * @format
  */
-
+import 'react-native-gesture-handler';
 import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
