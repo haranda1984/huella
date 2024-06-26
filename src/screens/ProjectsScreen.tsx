4faba9e5a8b80a71f470d07cf5afb62ad19d5983
heriberto <haranda@heicommunity.com>
2024-06-26 10:26:01 -0600

    "Agregar pantallas"

---
+++ src/screens/ProjectsScreen.tsx
@@ -0,0 +1,91 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Section,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function ProjectsScreen(): React.JSX.Element {
+
+  return (
+    <SafeAreaView style={{backgroundColor: Colors.whitee}}>
+      <StatusBar
+        barStyle={{backgroundColor: Colors.white}}
+        backgroundColor={{backgroundColor: Colors.white}}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{backgroundColor: Colors.white}}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default ProjectsScreen;
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -2,16 +2,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 
+import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import {View,TextInput,Button,Alert,StyleSheet} from 'react-native';
+import ProjectsScreen from './src/screens/ProjectsScreen';
+import LoginScreen from './src/screens/LoginScreen';
 import auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
 
-const reference = database().ref('http://10.0.2.2:9000');
+/*const reference = database().ref('http://10.0.2.2:9000');
 
 
 const RegisterScreen = () => {
- /*   const [email, setEmail] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [displayName, setDisplayName] = useState('');
     const [disable, setDisable] = useState('true');
@@ -19,7 +22,7 @@ const RegisterScreen = () => {
     const HandleSingUp = () => {
         auth()
         .createUserWithEmailAndPassword()
-    }*/
+    }
 
         reference.set({
             name: 'Nicolas',
@@ -29,8 +32,30 @@ const RegisterScreen = () => {
             displayName: 'Nicolas',
             disable: 'false',
 
+        });*/
+
+
+        const RegisterScreen = () => {
+            // Función para mostrar el mensaje de bienvenida
+            const showWelcomeMessage = () => {
+                Alert.alert(
+                    'Bienvenido al Registro',
+                    'Gracias por registrarte en nuestra aplicación. ¡Esperamos que disfrutes tu experiencia!'
+                );
+            };
+            // Renderización del componente
+            return (
+                <View style={styles.container}>
+                    {/* Botón para mostrar el mensaje de bienvenida */}
+                    <Button title="Mostrar Mensaje de Bienvenida" onPress={showWelcomeMessage} />
+                </View>
+            );
+        };
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
         });
-
-};
-
-export default RegisterScreen;
+        export default RegisterScreen;
